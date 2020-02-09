fb5be9954a3a0d4cf45329c74277346ccdef4953
MinexAutomation <minex.automation@gmail.com>
2020-02-08 16:42:20 -0800

    Added IStringlyTypedPathOperator extensions.

--- source/R5T.Cambridge.Extensions/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Cambridge.Extensions
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Cambridge.Extensions/Code/Extensions/IStringlyTypedPathOperatorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.Lombardy;
+
+
+namespace R5T.Cambridge.Extensions
+{
+    public static class IStringlyTypedPathOperatorExtensions
+    {
+        public static string GetSolutionFileProjectFileRelativePath(this IStringlyTypedPathOperator stringlyTypedPathOperator, string solutionFilePath, string projectFilePath)
+        {
+            var solutionDirectoryPath = stringlyTypedPathOperator.GetDirectoryPathForFilePath(solutionFilePath);
+
+            var projectFileRelativePath = stringlyTypedPathOperator.GetRelativePathDirectoryToFile(solutionDirectoryPath, projectFilePath);
+            return projectFileRelativePath;
+        }
+    }
+}
