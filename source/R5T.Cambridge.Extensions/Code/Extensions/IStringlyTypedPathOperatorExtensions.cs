818072923a16801bfd9c415fe907df499f59399e
MinexAutomation <minex.automation@gmail.com>
2020-02-10 11:15:44 -0800

    Add IStringlyTypedPathOperator extension to get the project file path from the uniquely formatted solution file path and relative project file path (the VS solution file contains project file paths relative to the solution file's directory path, not the solution file itself). Added documentation.

--- source/R5T.Cambridge.Extensions/Code/Extensions/IStringlyTypedPathOperatorExtensions.cs
+++ source/R5T.Cambridge.Extensions/Code/Extensions/IStringlyTypedPathOperatorExtensions.cs
@@ -7,12 +7,26 @@ namespace R5T.Cambridge.Extensions
 {
     public static class IStringlyTypedPathOperatorExtensions
     {
+        /// <summary>
+        /// Performs special solution directory path-relative formatting.
+        /// </summary>
         public static string GetSolutionFileProjectFileRelativePath(this IStringlyTypedPathOperator stringlyTypedPathOperator, string solutionFilePath, string projectFilePath)
         {
             var solutionDirectoryPath = stringlyTypedPathOperator.GetDirectoryPathForFilePath(solutionFilePath);
 
             var projectFileRelativePath = stringlyTypedPathOperator.GetRelativePathDirectoryToFile(solutionDirectoryPath, projectFilePath);
             return projectFileRelativePath;
+        }
+
+        /// <summary>
+        /// Performs special solution directory path-relative formatting.
+        /// </summary>
+        public static string GetProjectFilePath(this IStringlyTypedPathOperator stringlyTypedPathOperator, string solutionFilePath, string projectFileRelativePath)
+        {
+            var solutionFileDirectoryPath = stringlyTypedPathOperator.GetDirectoryPathForFilePath(solutionFilePath);
+
+            var projectFilePath = stringlyTypedPathOperator.Combine(solutionFileDirectoryPath, projectFileRelativePath);
+            return projectFilePath;
         }
     }
 }
